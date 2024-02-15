d807c899ce2beca6c2d1caa1dba43637787906d4
Fahad Mahmud <fahadmahmud.icte@gmail.com>
2024-02-16 02:43:54 +0600

    Initial commit

---
+++ zunaid/bin/zunaid.dart
@@ -0,0 +1,5 @@
+import 'package:zunaid/zunaid.dart' as zunaid;
+
+void main(List<String> arguments) {
+  print('Hello world: ${zunaid.calculate()}!');
+}
---
+++ zunaid/test/zunaid_test.dart
@@ -0,0 +1,8 @@
+import 'package:zunaid/zunaid.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
